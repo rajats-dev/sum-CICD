0ff8b07d5898eb147ee95698316ae0856f4b4a81
rajats-dev <rajatsundriyal1@gmail.com>
2025-12-05 11:13:00 +0530

    fixing the worklow

--- index.js
+++ index.js
@@ -3,41 +3,45 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-const todos = [{
-  id: 1,
-  title: "Todo 1",
-  description: "This is todo 1",
-  completed: false,
-}, {
-  id: 2,
-  title: "Todo 2",
-  description: "This is todo 2",
-  completed: false,
-}, {
-  id: 3,
-  title: "Todo 3",
-  description: "This is todo 3",
-  completed: false,
-
-}, {
-  id: 4,
-  title: "Todo 4",
-  description: "This is todo 4",
-  completed: false,
-}, {
-
-  id: 5,
-  title: "Todo 5",
-  description: "This is todo 5",
-  completed: false,
-}]
+const todos = [
+  {
+    id: 1,
+    title: "Todo 1",
+    description: "This is todo 1",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Todo 2",
+    description: "This is todo 2",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Todo 3",
+    description: "This is todo 3",
+    completed: false,
+  },
+  {
+    id: 4,
+    title: "Todo 4",
+    description: "This is todo 4",
+    completed: false,
+  },
+  {
+    id: 5,
+    title: "Todo 5",
+    description: "This is todo 5",
+    completed: false,
+  },
+];
 
 app.get("/todo", (req, res) => {
-  const todo = todos.find(t => t.id == req.query.id);
+  const todo = todos.find((t) => t.id == req.query.id);
   res.json({
-    todo
-  })
-})
+    todo,
+  });
+});
 
 app.get("/todos", (req, res) => {
   const randomTodos = [];
@@ -48,7 +52,7 @@ app.get("/todos", (req, res) => {
   }
   res.json({
     todos: randomTodos,
-  })
+  });
 });
 
 app.get("/sum", (req, res) => {
@@ -67,8 +71,7 @@ app.get("/interest", (req, res) => {
   res.send({
     total: total,
     interest: interest,
-  })
-
+  });
 });
 
 function getRandomNumber(max) {
@@ -80,9 +83,10 @@ app.get("/notifications", (req, res) => {
     network: getRandomNumber(10),
     jobs: getRandomNumber(10),
     messaging: getRandomNumber(10),
-    notifications: getRandomNumber(10)
-  })
+    notifications: getRandomNumber(10),
+  });
+});
 
-})
-
-app.listen(8080)
+app.listen(8080, () => {
+  console.log("Server runnning at 8080");
+});
